cfe77cfdb80339700e16f5c3c8d5bbaae54e8f56
Guillaume Allard <guillaume.allard@gmail.com>
2019-06-19 12:22:25 +0200

    Add tests

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 PRODUCTS = [
     { 'id': 1, 'name': 'Skello' },
-    { 'id': 2, 'name': 'Socialive.tv' }
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Le Wagon' }
 ]
 
 @app.route('/')
